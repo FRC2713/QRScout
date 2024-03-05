fa6809bccd98010210f71da071cb09826a50481d
Ty Tremblay <ty.tremblay@gmail.com>
2024-03-05 17:10:08 -0500

    start adding in-app code editor

---
+++ src/components/ConfigEditor.tsx
@@ -0,0 +1,13 @@
+import Editor from '@monaco-editor/react';
+import configJson from '../../config/2024/config.json';
+
+export function ConfigEditor() {
+  return (
+    <div className="fixed inset-0 h-full w-full overflow-y-auto bg-gray-500 bg-opacity-50 dark:bg-opacity-70 backdrop-blur ">
+      <Editor
+        defaultLanguage="json"
+        defaultValue={JSON.stringify(configJson)}
+      />
+    </div>
+  );
+}
