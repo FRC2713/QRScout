5d6566166c16866cd09aa58fdbabaa1b19ddc60b
Ty Tremblay <ty.tremblay@gmail.com>
2024-03-06 10:36:56 -0500

    add config editor

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -5,6 +5,7 @@ export function ConfigEditor() {
   return (
     <div className="fixed inset-0 h-full w-full overflow-y-auto bg-gray-500 bg-opacity-50 dark:bg-opacity-70 backdrop-blur ">
       <Editor
+        height="90vh"
         defaultLanguage="json"
         defaultValue={JSON.stringify(configJson)}
       />
