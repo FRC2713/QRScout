5c5e612b3c981680a04f913c6e25d12f02b3544d
Hillo111 <stas.gannutin@gmail.com>
2024-03-12 00:24:15 -0500

    Use buttons to show description

--- src/components/inputs/InputCard.tsx
+++ src/components/inputs/InputCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export interface InputCardProps {
   title: string
@@ -10,6 +10,7 @@ export interface InputCardProps {
 export default function InputCard(
   props: React.PropsWithChildren<InputCardProps>
 ) {
+  const [showDescription, setShowDescription] = useState(false);
   return (
     <div className="mx-1 rounded bg-white leading-tight shadow-sm dark:bg-gray-500">
       <div className="flex flex-row justify-between rounded-t bg-gray-300">
@@ -20,10 +21,20 @@ export default function InputCard(
           <p className="mr-1 h-4 w-4 font-rhr text-red-rhr">!!</p>
         )}
       </div>
-      <div>{props.children}</div>
-      {props.description != null && (
-        <div className="justify-between rounded-t bg-gray-200">
-          <p className="font-bold text-left text-s">Note</p>
+      <div style="position: relative">
+        {props.description != null && (
+          <button 
+            className="rounded bg-gray-300 text-xs p-0.5 absolute right-0 bottom-0"
+            type="button" 
+            onClick={() => { setShowDescription(!showDescription); }}
+          >
+          {showDescription ? "▲" : "▼"} Note
+          </button>
+        )}
+        <div>{props.children}</div>
+      </div>
+      {props.description != null && showDescription && (
+        <div className="justify-between rounded-t bg-gray-100 p-1">
           <p className="text-left text-xs">{props.description}</p>
         </div>
       )}
