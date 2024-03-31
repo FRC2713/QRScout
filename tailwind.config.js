f392cf84bda43c3dfde3c34f1b37655432931de4
Sasha <117695072+Hajajus001@users.noreply.github.com>
2024-03-30 17:21:14 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         'rhr-ns': ['"SF Sports Night NS"'],
       },
       colors: {
-        lightYellow: { rhr: '#fff200' },
+        red: { rhr: '#fff200' },
       },
     },
   },
