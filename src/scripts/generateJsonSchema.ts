2262174afbc1eb40829d563f9fc050ef27b986f0
Ty Tremblay <ty.tremblay@gmail.com>
2024-12-13 18:34:32 -0500

    schema generation adjustments

--- src/scripts/generateJsonSchema.ts
+++ src/scripts/generateJsonSchema.ts
@@ -1,6 +1,11 @@
+import { z } from 'zod';
 import { zodToJsonSchema } from 'zod-to-json-schema';
 import { configSchema } from '../components/inputs/BaseInputProps';
 
-const schema = zodToJsonSchema(configSchema);
+const schemaSchema = configSchema.extend({
+  $schema: z.string().optional(),
+});
+
+const schema = zodToJsonSchema(schemaSchema);
 
 console.log(JSON.stringify(schema, null, 2));
