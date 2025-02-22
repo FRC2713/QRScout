437c28a5b3b6d033b67555211880bfdd86cbb327
Emilio Alvarez <e.l.alvarez@icloud.com>
2025-02-22 12:50:13 -0600

    Remove base config from vite
    Testing a resolve path issue theory that we don't need to prepend QRscout as the subdomain is already doing the splitting

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QRScout/',
+  // base: '/QRScout/',
   plugins: [react()],
   resolve: {
     alias: {
