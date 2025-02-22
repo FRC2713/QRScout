fd3a909964024fa34b8b2788159a26b157d90679
Emilio Alvarez <e.l.alvarez@icloud.com>
2025-02-22 12:37:02 -0600

    Continue to remove statsig

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ import { ConfigSection } from './components/Sections/ConfigSection';
 import { ThemeProvider } from './components/ThemeProvider';
 import { useQRScoutState } from './store/store';
 
-import { StatsigProvider, useClientAsyncInit } from '@statsig/react-bindings';
+// import { StatsigProvider, useClientAsyncInit } from '@statsig/react-bindings';
 // import { runStatsigAutoCapture } from '@statsig/web-analytics';
 
 export function App() {
@@ -15,38 +15,36 @@ export function App() {
     teamNumber: state.formData.teamNumber,
     pageTitle: state.formData.page_title,
   }));
-  const { client } = useClientAsyncInit(
-    import.meta.env.VITE_STATSIG_CLIENT_KEY,
-    {
-      userID: `${teamNumber}`,
-    },
-  );
+  // const { client } = useClientAsyncInit(
+  //   import.meta.env.VITE_STATSIG_CLIENT_KEY,
+  //   {
+  //     userID: `${teamNumber}`,
+  //   },
+  // );
 
   // useEffect(() => {
   //   runStatsigAutoCapture(client);
   // }, [client]);
-
+  //<StatsigProvider client={client} loadingComponent={<div>Loading...</div>}>
   return (
-    <StatsigProvider client={client} loadingComponent={<div>Loading...</div>}>
-      <ThemeProvider>
-        <div className="min-h-screen py-2">
-          <Header />
-          <main className="flex flex-1 flex-col items-center justify-center px-4 text-center">
-            <h1 className="font-sans text-6xl font-bold">
-              <div className={`font-rhr text-primary`}>{pageTitle}</div>
-            </h1>
+    <ThemeProvider>
+      <div className="min-h-screen py-2">
+        <Header />
+        <main className="flex flex-1 flex-col items-center justify-center px-4 text-center">
+          <h1 className="font-sans text-6xl font-bold">
+            <div className={`font-rhr text-primary`}>{pageTitle}</div>
+          </h1>
 
-            <form className="w-full px-4" onSubmit={e => e.preventDefault()}>
-              <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-                <Sections />
-                <CommitAndResetSection />
-                <ConfigSection />
-              </div>
-            </form>
-          </main>
-          <Footer />
-        </div>
-      </ThemeProvider>
-    </StatsigProvider>
+          <form className="w-full px-4" onSubmit={e => e.preventDefault()}>
+            <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+              <Sections />
+              <CommitAndResetSection />
+              <ConfigSection />
+            </div>
+          </form>
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
