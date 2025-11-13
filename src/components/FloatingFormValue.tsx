bb7299924621a014c0813bd9b017f526295c1deb
Owen-Morgan825 <owen.morgan092@gmail.com>
2025-11-12 19:05:42 -0500

    Implement floating form value in a more generic way so that it can use the value of any form element

--- src/components/FloatingFormValue.tsx
+++ src/components/FloatingFormValue.tsx
@@ -1,10 +1,9 @@
 import { useQRScoutState } from "@/store/store";
 
 export function FloatingFormValue() {
-    const showTeamNumber = useQRScoutState(state => state.formData.showTeamNumber);
-    const teamNumber = useQRScoutState(state => state.fieldValues).filter(f => f.code === 'teamNumber')[0].value;
-    const className = "sticky top-5 w-1/2 sm:w-full space-y-1.5 p-2 bg-primary mb-2 rounded-xl leading-none text-primary-foreground font-rhr-ns" + ((showTeamNumber)? "block" : "hidden");
-    console.log(className);
+    const {showField, codeValue} = useQRScoutState(state => {return {showField: state.formData.floatingField.show, codeValue: state.formData.floatingField.codeValue}});
+    const teamNumber = useQRScoutState(state => state.fieldValues).filter(f => f.code === codeValue)[0].value;
+    const className = "sticky top-5 w-1/2 sm:w-full space-y-1.5 p-2 bg-primary mb-2 rounded-xl leading-none text-primary-foreground font-rhr-ns " + ((showField)? "block" : "hidden");
     return (
         <div
         className={className}>
--- src/components/inputs/BaseInputProps.ts
+++ src/components/inputs/BaseInputProps.ts
@@ -174,9 +174,12 @@ export const configSchema = z.object({
   teamNumber: z
     .number()
     .describe('The team number of the team using this form.'),
-  showTeamNumber: z
-    .boolean()
-    .describe('Whether or not to always show the selected team number at the top of the screen. May be useful on small screens'),
+  floatingField: z
+    .object({
+      show: z.boolean().describe('Whether or not to always show this value at the top of the screen. May be useful on small screens'),
+      codeValue: z.string().describe('Code of the form field to get this value from')
+    })
+    .describe('Optional floating text box at the tob of the screen to show things like the team number. May be useful on small screens'),
   theme: themeSchema.default({
     light: {
       background: '0 0% 100%',
