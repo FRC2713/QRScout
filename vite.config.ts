26786a703d183ff9c7fb49f34edb3c803724f412
Pranay Sanghai <pranaysanghai@gmail.com>
2026-03-05 17:05:02 -0800

    Fix repo URLs

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QRScout/',
+  base: '/QRScout-702/',
   plugins: [
     react(),
     VitePWA({
