d241bfea94b16baca4453925542f8fa55a158885
luanzeba <luanzeba@users.noreply.github.com>
2026-03-12 20:04:56 +0000

    Deploy preview for PR 114 🛫

---
+++ pr-preview/pr-114/assets/flag-triangle-left-C4H2D81e.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-CRgV1P7T.js";const r=e("FlagTriangleLeft",[["path",{d:"M17 22V2L7 7l10 5",key:"1rmf0r"}]]);export{r as default};
---
+++ pr-preview/pr-114/assets/tally-1-D1KISGV7.js
@@ -0,0 +1,1 @@
+import{c as a}from"./index-CRgV1P7T.js";const l=a("Tally1",[["path",{d:"M4 4v16",key:"6qkkli"}]]);export{l as default};
