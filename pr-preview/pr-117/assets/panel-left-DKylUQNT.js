cb7cd32697bc2a1180856a6e862f170804272b27
Noah-It-All <Noah-It-All@users.noreply.github.com>
2026-03-29 12:22:57 +0000

    Deploy preview for PR 117 🛫

---
+++ pr-preview/pr-117/assets/panel-left-DKylUQNT.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-B4V70BsE.js";const a=e("PanelLeft",[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}],["path",{d:"M9 3v18",key:"fh3hqa"}]]);export{a as default};
